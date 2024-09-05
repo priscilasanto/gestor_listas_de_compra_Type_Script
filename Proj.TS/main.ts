3852b52dcfe22d2562f26e2d6e722755433d1c41
maria-larissaO <marialarissa1415@gmail.com>
2024-09-05 16:37:20 -0300

    add arquivos

---
+++ Proj.TS/main.ts
@@ -0,0 +1,37 @@
+import { menu, adicionar_item, remover_item, editar_item, marcar_comprado, listar_itens, resumo_lista,} from './gestor.ts';
+  
+  while (true) {
+    menu();
+    const opcao = Number(prompt('Digite a opção: '));
+  
+    switch (opcao) {
+      case 1:
+        adicionar_item();
+        break;
+        case 2:
+            remover_item();
+            break;
+        case 3:
+            editar_item();
+            break;
+        case 4:
+            marcar_comprado();
+            break;
+        case 5:
+            listar_itens();
+            break;
+        case 6:
+            resumo_lista();
+            break;
+      case 7:
+        console.log('Saindo...');
+        break;
+      default:
+        console.log('Opção inválida!!!');
+    }
+  
+    if (opcao === 7) {
+      console.log('Você saiu do programa! \nObrigado por nos escolher!!!');
+      break;
+    }
+  }
